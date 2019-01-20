3efe8c01856ddb1a29d41629a66afb1e940015e5
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-01-20 14:22:08 +0700

    Add Full unit test for case SingleTon

--- app/src/test/java/com/example/anhvietpham/basic_unit_test/singleton/FitnessTrackerTest.kt
+++ app/src/test/java/com/example/anhvietpham/basic_unit_test/singleton/FitnessTrackerTest.kt
@@ -1,5 +1,7 @@
 package com.example.anhvietpham.basic_unit_test.singleton
 
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
 
@@ -14,7 +16,14 @@ class FitnessTrackerTest{
 
     @Test
     fun step_totalIncremented() {
+        SUT.step()
+        assertThat(SUT.getTotalSteps(), `is`(1))
+    }
 
+    @Test
+    fun runStep_totalIncrementedByCorrectRatio(){
+        SUT.runStep()
+        assertThat(SUT.getTotalSteps(), `is`(2))
     }
 
 
