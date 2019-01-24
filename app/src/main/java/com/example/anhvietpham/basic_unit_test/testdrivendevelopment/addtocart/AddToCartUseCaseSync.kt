e8d63cd9546352a8122d1b551a969559fcd0648a
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-01-24 09:27:56 +0700

    ADD test case addToCartSync_correctParameterPassedToEndpoint

--- app/src/main/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/addtocart/AddToCartUseCaseSync.kt
+++ app/src/main/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/addtocart/AddToCartUseCaseSync.kt
@@ -3,6 +3,10 @@ package com.example.anhvietpham.basic_unit_test.testdrivendevelopment.addtocart
 import com.example.anhvietpham.basic_unit_test.testdrivendevelopment.addtocart.network.AddToCartHttpEndpointSync
 
 class AddToCartUseCaseSync(private val addToCartHttpEndpointSync: AddToCartHttpEndpointSync) {
+    fun addToCartSync(offerid: String, amount: Int) {
+
+    }
+
     enum class UseCaseResult{
         SUCCESS,
         FAILURE,
--- app/src/test/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/addtocart/AddToCartUseCaseSyncTest.kt
+++ app/src/test/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/addtocart/AddToCartUseCaseSyncTest.kt
@@ -2,8 +2,14 @@ package com.example.anhvietpham.basic_unit_test.testdrivendevelopment.addtocart
 
 import com.example.anhvietpham.basic_unit_test.testdrivendevelopment.addtocart.network.AddToCartHttpEndpointSync
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentCaptor
 import org.mockito.Mockito.`when`
 
 const val OFFERID = "offerId"
@@ -19,6 +25,17 @@ class AddToCartUseCaseSyncTest{
     }
 
     // correct parameters passed to the endpoint
+    @Test
+    fun addToCartSync_correctParameterPassedToEndpoint() {
+        // Arrange
+        val ac = argumentCaptor<CartItemScheme>()
+        // Act
+        SUT.addToCartSync(OFFERID, AMOUNT)
+        // Assert
+        verify(mAddToCartHttpEndpointSyncMock).addToCartSync(ac.capture())
+        Assert.assertThat(ac.firstValue.mOfferId, `is`(OFFERID))
+        Assert.assertThat(ac.firstValue.mAmount, `is`(AMOUNT))
+    }
     // endpoint success - success returned
     // endpoint auth error - failure returned
     // endpoint general error - failure returned
