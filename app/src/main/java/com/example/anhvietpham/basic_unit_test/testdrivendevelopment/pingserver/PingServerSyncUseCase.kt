aa2142b7750e2ba46e37bcff7313a1226ab95b85
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-01-24 17:18:59 +0700

    ADD pingServerSync_success_successReturned

--- app/src/main/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/pingserver/PingServerSyncUseCase.kt
+++ app/src/main/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/pingserver/PingServerSyncUseCase.kt
@@ -2,4 +2,11 @@ package com.example.anhvietpham.basic_unit_test.testdrivendevelopment.pingserver
 
 class PingServerSyncUseCase {
 
+    fun pingServerSync() : UseCaseResult {
+        return UseCaseResult.SUCCESS
+    }
+
+    enum class UseCaseResult{
+        SUCCESS
+    }
 }
--- app/src/test/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/pingserver/PingServerSyncUseCaseTest.kt
+++ app/src/test/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/pingserver/PingServerSyncUseCaseTest.kt
@@ -1,13 +1,34 @@
 package com.example.anhvietpham.basic_unit_test.testdrivendevelopment.pingserver
 
+import com.example.anhvietpham.basic_unit_test.testdrivendevelopment.pingserver.network.PingServerHttpEndPointSync
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
 
 class PingServerSyncUseCaseTest{
+    private val mPingServerHttpEndPointSync: PingServerHttpEndPointSync = mock()
     private lateinit var SUT: PingServerSyncUseCase
 
     @Before
     fun setup(){
         SUT = PingServerSyncUseCase()
+        success()
+    }
+
+    @Test
+    fun pingServerSync_success_successReturned() {
+        // Arrange
+        // Act
+        val result = SUT.pingServerSync()
+        // Assert
+        assertThat(result,`is`(PingServerSyncUseCase.UseCaseResult.SUCCESS))
+    }
+
+    private fun success() {
+        whenever(mPingServerHttpEndPointSync.pingServerSync()).thenReturn(PingServerHttpEndPointSync.EndPointResult.SUCCESS)
     }
 }
