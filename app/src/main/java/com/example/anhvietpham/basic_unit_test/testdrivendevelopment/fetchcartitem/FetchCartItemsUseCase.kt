d53ab254a47e55cbdcd8b1b000f95304a91748c5
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-01-25 09:42:25 +0700

    Add unit test fetchCartItems_correctLimitPassedToEndPoint()

--- app/src/main/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/fetchcartitem/FetchCartItemsUseCase.kt
+++ app/src/main/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/fetchcartitem/FetchCartItemsUseCase.kt
@@ -1,4 +1,19 @@
 package com.example.anhvietpham.basic_unit_test.testdrivendevelopment.fetchcartitem
 
-class FetchCartItemsUseCase {
+import com.example.anhvietpham.basic_unit_test.testdrivendevelopment.fetchcartitem.cart.CartItemSchema
+import com.example.anhvietpham.basic_unit_test.testdrivendevelopment.fetchcartitem.network.GetCartItemsHttpEndpoint
+
+class FetchCartItemsUseCase(private val getCartItemsHttpEndpoint: GetCartItemsHttpEndpoint) {
+    fun fetchCartItemsAndNotify(limit: Int) {
+        getCartItemsHttpEndpoint.getCartItems(limit, object : GetCartItemsHttpEndpoint.CallBack{
+            override fun onGetCartItemSuccessed(cartItems: List<CartItemSchema>) {
+
+            }
+
+            override fun onGetCartItemsFailed(failReason: GetCartItemsHttpEndpoint.FailReason) {
+
+            }
+        })
+    }
+
 }
--- app/src/test/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/fetchcartitem/FetchCartItemsUseCaseTest.kt
+++ app/src/test/java/com/example/anhvietpham/basic_unit_test/testdrivendevelopment/fetchcartitem/FetchCartItemsUseCaseTest.kt
@@ -1,14 +1,42 @@
 package com.example.anhvietpham.basic_unit_test.testdrivendevelopment.fetchcartitem
 
+import com.example.anhvietpham.basic_unit_test.testdrivendevelopment.fetchcartitem.network.GetCartItemsHttpEndpoint
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 
+const val LIMIT = 10
 class FetchCartItemsUseCaseTest{
+
+    private val mGetCartItemsHttpEndpoint: GetCartItemsHttpEndpoint = mock()
+
     private lateinit var SUT: FetchCartItemsUseCase
 
     @Before
     fun setUp(){
-        SUT = FetchCartItemsUseCase()
+        SUT = FetchCartItemsUseCase(getCartItemsHttpEndpoint = mGetCartItemsHttpEndpoint)
     }
 
+    // Correct limit passed to the endpoint
+    @Test
+    fun fetchCartItems_correctLimitPassedToEndPoint() {
+        // Arrange
+        val actInt = argumentCaptor<Int>()
+        // Act
+        SUT.fetchCartItemsAndNotify(LIMIT)
+        // Assert
+        verify(mGetCartItemsHttpEndpoint).getCartItems(actInt.capture(), any())
+        Assert.assertThat(actInt.firstValue, `is`(LIMIT))
+    }
 
+    // Success - all observer notified with correct data
+    // Success - unsubscribed observers not notified
+    // General error - observer notified of failure
+    // Network error - observer notified of failure
+    //
 }
